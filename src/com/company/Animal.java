92769e48772916d59e1b85bc4ac95860cd30380a
Ivkoncho <51968910+Ivkoncho@users.noreply.github.com>
2022-03-22 20:23:22 +0200

    parents and childrens

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,5 +1,35 @@
 package com.company;
 
 public class Animal {
+    public String name;
+    private  String Breed;
+    private int age;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return Breed;
+    }
+
+    public void setBreed(String breed) {
+        Breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 
 }
--- src/com/company/Cat.java
+++ src/com/company/Cat.java
@@ -1,37 +1,9 @@
 package com.company;
 
-public class Cat {
-
-      private  String name;
-        private  String Breed;
-        private int age;
+public class Cat extends Animal {
 
         public Cat(String name) {
             this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getBreed() {
-            return Breed;
-        }
-
-        public void setBreed(String breed) {
-            Breed = breed;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
         }
 
         public String makeSound(){
