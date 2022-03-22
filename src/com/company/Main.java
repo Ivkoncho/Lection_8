4151da00320c7805777d2f7e4f5035fa1bf0cba2
Ivkoncho <51968910+Ivkoncho@users.noreply.github.com>
2022-03-22 20:11:52 +0200

    improvemnts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,17 @@ public class Main {
 	// write your code here
        dog Dog = new dog("Rex");
        System.out.println(Dog.makeSound());
+       Dog.setAge(5);
+       System.out.println(Dog.getAge());
+       Dog.setBreed("koker");
+        System.out.println(Dog.getBreed());
 
+        Cat Cat = new Cat("Komo");
+        System.out.println(Cat.makeSound());
+        Cat.setAge(5);
+        System.out.println(Cat.getAge());
+       Cat.setBreed("British");
+        System.out.println(Cat.getBreed());
 
     }
 }
